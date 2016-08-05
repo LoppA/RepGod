22be28b6fc304f16604e028c2ee3c3dcc31262a6
LoppA <Lopacheco@outlook.com>
2016-08-05 11:23:23 -0300

    base.cpp

---
+++ base.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+
+typedef pair<int, int> ii;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+const double E = exp(1);
+
+int main (void) {
+	ios_base::sync_with_stdio(false);
+
+	return 0;
+}
