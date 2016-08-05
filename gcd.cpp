284dc7701ccc2a9761d3f1a80fbb205fc24fa19f
LoppA <Lopacheco@outlook.com>
2016-08-05 13:10:35 -0300

    gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef pair<int, int> ii;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+const double E = exp(1);
+
+ll gcd (ll a, ll b) {
+	if (!b)
+		return a;
+	else
+		return gcd(b, a%b);
+}
+
+int main (void) {
+	ios_base::sync_with_stdio(false);
+
+	return 0;
+}
