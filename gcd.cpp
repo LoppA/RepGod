6cb3b5c3278ed9f998f7601c953460371553c619
LoppA <Lopacheco@outlook.com>
2016-08-09 17:01:48 -0300

    menor

--- gcd.cpp
+++ gcd.cpp
@@ -2,16 +2,7 @@
 
 using namespace std;
 
-#define pb push_back
-#define mk make_pair
-#define fi first
-#define se second
-
 typedef long long ll;
-typedef pair<int, int> ii;
-const int INF = 0x3f3f3f3f;
-const double PI = acos(-1.0);
-const double E = exp(1);
 
 ll gcd (ll a, ll b) {
 	if (!b)
@@ -21,7 +12,5 @@ ll gcd (ll a, ll b) {
 }
 
 int main (void) {
-	ios_base::sync_with_stdio(false);
-
 	return 0;
 }
