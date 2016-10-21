b2e47761c3447fd26c3e4c1fcffe8ba56330f925
LoppA <USUARIOS9293182@ltsp824.(none)>
2016-10-21 18:37:03 -0200

    go

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,6 +1,3 @@
-/* UVA 11228
-   MST - Kruskal  O (M*log(N))	*/
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -15,7 +12,7 @@ const int INF = 0x3f3f3f3f;
 const double PI = acos(-1.0);
 const double E = exp(1);
 
-const int N = 1e3 + 4;
+const int N = 1e5 + 4;
 
 class uni {
 public:
@@ -57,68 +54,28 @@ public:
 	int comps () { return cps; }
 };
 
-int n;
-double r;
-ii p[N];
+vector <pair<int, ii> > edg;
+vector <ii> mst;
 
-double dist (ii p1, ii p2) {
-	return sqrt((p1.fi - p2.fi)*(p1.fi - p2.fi) + (p1.se - p2.se)*(p1.se - p2.se));
-}
-
-uni U1;
-int go1 () {
-	U1 = uni(n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (i == j) continue;
-			if (dist(p[i], p[j]) <= r) {
-				U1.une(i, j);
-			}
-		}
-	}
-
-	return U1.comps();
-}
-
-vector <pair<double, pair<int, int> > > edg;
-
-void kruskal () {
-	edg.clear();
-	uni U2 = uni(n);
-	double r1 = 0, r2 = 0;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = i+1; j < n; j++) {
-			edg.pb(mk(dist(p[i], p[j]),mk(i,j)));
-		}
-	}
+int kruskal () {
 	sort(edg.begin(), edg.end());
+	int ret = 0;
+	uni U;
 
-	for (int i = 0; i < edg.size(); i++) {
+	for (int i = 0; i < (int)edg.size(); i++) {
 		int u = edg[i].se.fi, v = edg[i].se.se;
-		double dist = edg[i].fi;
-		if (U2.une(u,v)) {
-			if (U1.find(u) != U1.find(v)) r2 += dist;
-			else r1 += dist;
+		int dist = edg[i].fi;
+		if (U.une(u,v)) {
+			mst.pb(mk(u,v));
+			ret += dist;
 		}
 	}
-	cout << setprecision(0) << fixed  << r1 << " " << r2 << endl;
+
+	return ret;
 }
 
 int main (void) {
 	ios_base::sync_with_stdio(false);
-
-	int t;	cin >> t;
-
-	for (int T = 1; T <= t; T++) {
-		cin >> n >> r;
-		for (int i = 0; i < n; i++) {
-			cin >> p[i].fi >> p[i].se;
-		}
-		cout << "Case #" << T << ": ";
-		cout << go1() << " ";
-		kruskal();
-	}
 
 	return 0;
 }
