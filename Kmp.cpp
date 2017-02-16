15bb651cadbf5417e6207f524b907319e01dd60e
LoppA <Lopacheco@outlook.com>
2017-02-16 00:16:48 -0200

    Mike KMP

--- Kmp.cpp
+++ Kmp.cpp
@@ -2,43 +2,59 @@
 
 using namespace std;
 
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef pair<int, int> ii;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+
 const int N = 1e6 + 6;
-int b[N];
-int m, n;
+int pi[N];
 string p, t;
+
+void pre () {
+	p += '#';
+
+	pi[0] = pi[1] = 0;
+	for (int i = 2; i <= (int)p.size(); i++) {
+		pi[i] = pi[i-1];
+
+		while (pi[i] > 0 and p[pi[i]] != p[i-1])
+			pi[i] = pi[pi[i]];
+
+		if (p[pi[i]] == p[i-1])
+			pi[i]++;
+	}
+}
 
 void report (int at) {
 
 }
 
-void KMPPre () {
-	int i = 0, j = -1;
-	b[0] = -1;
-	while (i < m) {
-		while (j >= 0 and p[i] != p[j])	j = b[j];
-		i++;	j++;
-		b[i] = j;
-	}
-}
-
 void KMP () {
-	m = p.size();
-	n = t.size();
+	pre ();
 
-	KMPPre();
+	int k = 0;
+	int m = p.size() - 1;
 
-	int i = 0, j = 0;
-	while (i < n) {
-		while (j >= 0 and t[i] != p[j])	j = b[j];
-		i++;	j++;
-		if (j == m) {
-			report (i - j);
-			j = b[j];
-		}
+	for (int i = 0; i < (int)t.size(); i++) {
+		while (k > 0 and p[k] != t[i])
+			k = pi[k];
+		
+		if (p[k] == t[i])
+			k++;
+		if (k == m)
+			report (i - m);
 	}
+
 }
 
 int main (void) {
+	ios_base::sync_with_stdio(false);
 
 	return 0;
 }
