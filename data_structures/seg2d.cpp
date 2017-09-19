c805fc9515f609f41fb6176db99403729f781967
LoppA <xLopacheco@gmail.com>
2017-09-19 11:06:50 -0300

    codigo menor, update correto

--- data_structures/seg2d.cpp
+++ data_structures/seg2d.cpp
@@ -1,4 +1,6 @@
 // LIS2    http://codeforces.com/gym/101055/problem/D SOLUTION
+//
+// POINT UPDATE, RANGE QUERY
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -19,146 +21,101 @@ const double E = exp(1);
 
 struct no {
 	int *seg;
-	vector <int> v;
+	vector <ii> v;
 	int n;
-	int a, b;
+	int ay, by, a, b;
 
 	no () {}
 
-	no (int sz) {
-		n = sz;
+	no (int n) : n(n) {
 		seg = new int [4*n + 1];
 		for (int i = 0; i < 4*n + 1; i++)
 			seg[i] = 0;
+		v.resize(n);
 	}
 
-	void merge (vector <int> &v1, vector <int> &v2) {
-		int i = 0, j = 0;
-		while (i < (int)v1.size() and j < (int)v2.size()) {
-			if (v1[i] <= v2[j]) 
-				v.pb(v1[i++]);
-			else 
-				v.pb(v2[j++]);
-		}
-		while (i < (int)v1.size())
-			v.pb(v1[i++]);
-		while (j < (int)v2.size())
-			v.pb(v2[j++]);
+	int query (int ly, int ry, int l, int r, int val) {
+		ay = ly;	by = ry;
+		a = l;	b = r;
+		return query (1, 0, n-1, val);
 	}
 
-	int query (int y) {
-		y = upper_bound(v.begin(), v.end(), y) - v.begin() - 1;
-		a = 0;	b = min(y, (int)v.size() - 1);
-		return query (1, 0, n-1);
-	}
+	int query (int r, int i, int j, int val) {
+		if (v[j] < ii(ay, a) or v[i] > ii(by, b)) return 0;
 
-	int query (int r, int i, int j) {
-		if (j < a or i > b) return 0;
-
-		if (i >= a and j <= b) {
+		if (v[i] >= ii(ay, a) and v[j] <= ii(by, b)) {
+			seg[r] = max (seg[r], val);
 			return seg[r];
 		} else {
 			int mid = (i + j)/2;
-			int L = query (L(r), i, mid);
-			int R = query (R(r), mid + 1, j);
+			int L = query (L(r), i, mid, val);
+			int R = query (R(r), mid + 1, j, val);
+			seg[r] = max (seg[L(r)], seg[R(r)]);
 			return max(L, R);
-		}
-	}
-
-	void update (int y, int val) {
-		y = upper_bound(v.begin(), v.end(), y) - v.begin() - 1;
-		a = b = y;
-		update (1, 0, n-1, val);
-	}
-
-	void update (int r, int i, int j, int val) {
-		if (j < a or i > b) return;
-		
-		if (i >= a and j <= b) {
-			seg[r] = max(seg[r], val);
-		} else {
-			int mid = (i + j)/2;
-			update (L(r), i, mid, val);
-			update (R(r), mid + 1, j, val);
-			seg[r] = max(seg[L(r)], seg[R(r)]);
 		}
 	}
 };
 
 const int N = 1e5 + 5;
 int n;
-ii p[N], v[N];
-map <int, int> id;
-map <ii, int> noid;
+ii v[N], p[N];
 no seg[4*N + 1];
 
 void build (int r, int i, int j) {
 	seg[r] = no (j - i + 1);
 
 	if (i == j) {
-		seg[r].v.pb(v[i].se);
+		seg[r].v[0] = ii(v[i].se, v[i].fi);
 	} else {
 		int mid = (i + j)/2;
 		build (L(r), i, mid);
 		build (R(r), mid + 1, j);
-		seg[r].merge (seg[L(r)].v, seg[R(r)].v);
+
+		merge (seg[L(r)].v.begin(), seg[L(r)].v.end(), 
+		seg[R(r)].v.begin(), seg[R(r)].v.end(), seg[r].v.begin());
 	}
 }
 
-int a, b, y;
+int a, b, ay, by;
 
-int query (int r, int i, int j) {
-	if (j < a or i > b) return 0;
+int query (int r, int i, int j, int val) {
+	if (v[j].fi < a or v[i].fi > b) return 0;
 
-	if (i >= a and j <= b) {
-		return seg[r].query(y);
+	if (v[i].fi >= a and v[j].fi <= b) {
+		return seg[r].query(ay, by, a, b, val);
 	} else {
 		int mid = (i + j)/2;
-		int L = query (L(r), i, mid);
-		int R = query (R(r), mid + 1, j);
+		int L = query (L(r), i, mid, val);
+		int R = query (R(r), mid + 1, j, val);
+		seg[r].query(ay, by, a, b, val);
 		return max(L, R);
 	}
-}
-
-void update (int r, int i, int j, int val) {
-	if (j < a or i > b) return;
-
-	seg[r].update(y, val);
-
-	if (i == j) return;
-	int mid = (i + j)/2;
-	update (L(r), i, mid, val);
-	update (R(r), mid + 1, j, val);
 }
 
 int main (void) {
 	scanf ("%d", &n);
 
 	for (int i = 0; i < n; i++) {
-		scanf ("%d%d", &p[i].fi, &p[i].se);
+		scanf ("%d %d", &p[i].fi, &p[i].se);
 		v[i] = p[i];
 	}
 
 	sort (v, v + n);
-
-	for (int i = 0; i < n; i++) {
-		id[v[i].fi] = i;
-		noid[v[i]] = i;
-	}
-	
 	build (1, 0, n - 1);
 
 	int res = 0;
+	/*V DEVE ESTAR ORDENADO PARA AS QUERIES FUNCIONAREM*/
 	for (int i = 0; i < n; i++) {
-		a = 0;	b = id[p[i].fi];
-		y = p[i].se;
-		int x = query (1, 0, n - 1);	
+		a = 0;	b = p[i].fi;
+		ay = 0;	by = p[i].se;
+
+		int x = query (1, 0, n - 1, 0);	
 
 		res = max (res, x + 1);
 
-		a = b = noid[p[i]];
-		y = p[i].se;
-		update (1, 0, n - 1, x + 1);
+		a = b = p[i].fi;
+		ay = by = p[i].se;
+		query (1, 0, n - 1, x + 1);
 	}
 
 	printf ("%d\n", res);
