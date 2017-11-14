a924b1bd82af04cca4f996138bbee6fbe369969b
LoppA <xLopacheco@gmail.com>
2017-11-14 11:35:57 -0200

    check if file exists

--- judge.py
+++ judge.py
@@ -1,22 +1,28 @@
+# github.com/guitubone
+# Samuel Ferreira
+# github.com/LoppA
+
 import os
 import sys
 
 wa = -1
-for i in range (1, 65) :
-	file_name = 'M_' + str(i);
+
+for i in range (1, 285) :
+	file_name = 'K_' + str(i);
 	input_file = 'input/' + file_name
 	output_file = 'output/' + file_name
-	cmd = './a.out <' + input_file + ' >out'
-	os.system(cmd)
-	cmd = 'diff ' + output_file + ' out >diff'
-	os.system(cmd)
-        print ('Judging test case ' + str(i))
-	if (os.stat("diff").st_size != 0) :
-		if (wa == -1) :
-			wa = i
-		print ('\tNO')
-	else :
-	    print ('\tOK')
+	if (os.path.isfile(input_file)) :
+		cmd = './a.out <' + input_file + ' >out'
+		os.system(cmd)
+		cmd = 'diff ' + output_file + ' out >diff'
+		os.system(cmd)
+		print ('Judging test case ' + str(i))
+		if (os.stat("diff").st_size != 0) :
+			if (wa == -1) :
+				wa = i
+			print ('\tNO')
+		else :
+			print ('\tOK')
 
 print("\n-----------------------------------------\n")
 if (wa == -1) :
