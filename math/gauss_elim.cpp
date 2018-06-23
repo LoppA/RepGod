4570982ec50e9b34016b2720920cd2291a0e5e4b
LoppA <xLopacheco@gmail.com>
2018-06-22 21:39:15 -0300

    added gauss elim

---
+++ math/gauss_elim.cpp
@@ -0,0 +1,105 @@
+// emaxx's gauss elim
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define mk make_pair
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef pair<int, int> ii;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+
+const int N = 500;
+const double EPS = 1e-9;
+
+// O(n*m*min(n,m))
+int gauss (vector < vector<double> > a, vector<double> & ans) {
+    int n = (int) a.size();
+    int m = (int) a[0].size() - 1;
+
+    vector<int> where (m, -1);
+    for (int col=0, row=0; col<m && row<n; ++col) {
+        int sel = row;
+        for (int i=row; i<n; ++i)
+            if (abs (a[i][col]) > abs (a[sel][col]))
+                sel = i;
+        if (abs (a[sel][col]) < EPS)
+            continue;
+        for (int i=col; i<=m; ++i)
+            swap (a[sel][i], a[row][i]);
+        where[col] = row;
+
+        for (int i=0; i<n; ++i)
+            if (i != row) {
+                double c = a[i][col] / a[row][col];
+                for (int j=col; j<=m; ++j)
+                    a[i][j] -= a[row][j] * c;
+            }
+        ++row;
+    }
+
+    ans.assign (m, 0);
+    for (int i=0; i<m; ++i)
+        if (where[i] != -1)
+            ans[i] = a[where[i]][m] / a[where[i]][i];
+    for (int i=0; i<n; ++i) {
+        double sum = 0;
+        for (int j=0; j<m; ++j)
+            sum += ans[j] * a[i][j];
+        if (abs (sum - a[i][m]) > EPS)
+            return 0;
+    }
+
+    for (int i=0; i<m; ++i)
+        if (where[i] == -1)
+            return INF;
+    return 1;
+}
+
+// mod 2 gauss
+int gauss (vector < bitset<N> > a, int n, int m, bitset<N> & ans) {
+    vector<int> where (m, -1);
+    for (int col=0, row=0; col<m && row<n; ++col) {
+        for (int i=row; i<n; ++i)
+            if (a[i][col]) {
+                swap (a[i], a[row]);
+                break;
+            }
+        if (! a[row][col])
+            continue;
+        where[col] = row;
+
+        for (int i=0; i<n; ++i)
+            if (i != row && a[i][col])
+                a[i] ^= a[row];
+        ++row;
+    }
+
+	for(int i = 0; i < n; i++) {
+		int sum = 0;
+		for(int j = 0; j < m; j++)
+			sum += a[i][j];
+		if(!sum and a[i][m])
+			return 0;
+	}
+
+	ans.reset();
+    for (int i=0; i<m; ++i)
+        if (where[i] != -1)
+            ans[i] = a[where[i]][m];
+
+    for (int i=0; i<m; ++i)
+        if (where[i] == -1)
+            return INF;
+
+    return 1;
+}
+
+int main(void) {
+	return 0;
+}
