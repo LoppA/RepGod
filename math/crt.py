30f2eaca4dc072e98518df54e49cad4c88dfad55
LoppA <xLopacheco@gmail.com>
2019-10-04 14:34:20 -0300

    removed wrong code

--- math/crt.py
+++ math/crt.py
@@ -14,34 +14,14 @@ def pot(x, y, mod):
 
     return ret
 
-# p = p0 * p1 * ... * p(n-1) pi are pairwise relatively prime
-# a = a0     (mod p0)
-# a = a1     (mod p1) ...
-# a = a(n-1) (mod p(n-1))
-# find a given pi and ai
-# a = sum[0 <= i < n]{ ai * (p/pi) * (pi/p)%pi } % p
-# O(N * log2(p))
-def crt(a, p):
-    # a and p are numpy.arrays
-    assert(a.size == p.size)
-    n = a.size
-    _p = 1
-    for it in p:
-        _p *= it
-    res = 0
-    for i in range (n):	
-        at = a[i] * ((_p/p[i]) * pot(_p/p[i], p[i]-2, p[i]))%_p
-        res = (res + at)%_p
-    return res
-
-# O(N**2 * log2(p)) but works with bultin types until final calculations
+# O(N**2 * log2(p)), works with bultin types until final calculations
 # Define: a = x0 + x1*p0 + x2*p0*p1 + ... + x[n-1]*p0*...*p[n-2]
 # So:  a = x0 (mod p0)
 #      a = x0 + x1*po (mod p1)
 # So:  x0 = a0 (mod p0)
 #      x1 = (a1 - x0)*p0**-1 (mod p1)
 #      x2 = ((a2 - x0)*p0**-1 - x1)*p1**-1 (mod p2)
-def crt2(a, p):
+def crt(a, p):
     # a and p are numpy.arrays
     assert(a.size == p.size)
     n = a.size
