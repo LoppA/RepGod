fed9008081b6659c0686e4513587f4fbcc51ea03
LoppA <xLopacheco@gmail.com>
2023-09-30 14:27:23 +0100

    Add debug macros

--- base.cpp
+++ base.cpp
@@ -14,6 +14,39 @@ typedef pair<int, int> ii;
 const int INF = 0x3f3f3f3f;
 const double PI = acos(-1.0);
 
+template <typename T>
+std::ostream& operator<< (std::ostream& out, const vector<T>& v) {
+    out << '[';
+    bool f = false;
+    for(auto& x:v){
+      if (f)  out<<", ";
+      f = true;
+      out<<x;
+    }
+    out << "]";
+    return out;
+}
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+  vector<string> __attribute__ ((unused)) args,
+  __attribute__ ((unused)) int idx,
+  __attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
 int main (void) {
 	ios_base::sync_with_stdio(false);
 
